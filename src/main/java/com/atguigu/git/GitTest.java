cee9baf5f45728647b70bf462f4ab7a726f2a256
zjj <zjj@0802.com>
2022-08-20 17:28:24 +0800

    hot-fix two commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git23");
         System.out.println("hot-fix1");
-
+        System.out.println("hot-fix2");
     }
 }
