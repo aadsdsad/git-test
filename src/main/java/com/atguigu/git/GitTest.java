85f1384580c5ce0ed7920e9014688e1ae01dc51c
zjj <zjj@0802.com>
2022-08-20 17:29:23 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git23");
         System.out.println("hot-fix1");
+        System.out.println("master test");
 
     }
 }
